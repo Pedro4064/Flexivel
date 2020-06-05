e507fde57abc11ce2a281200515f18ae5ef32392
Pedro Cruz <pedrohlcruz@gmail.com>
2020-06-05 20:50:28 -0300

    Translated the measuring code to OPP

---
+++ Sensor.h
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Arduino.h>
+#include <MAX5481.h>
+#include <LMP91000.h>
+
+
+class Sensor{
+
+    private:
+        
+        signed char determineLMP91000Bias(double Voltage);
+
+    public: 
+        Sensor();
+        void VoltametriaCiclica(double passo, double tensaoIni, double tensaoFin, double tensaoInv, double velocidadeScan, int NumCiclos);
+        void Amperometria(double tensaoIni, double tensao, int tempo, int intervalo, double frequencia);
+};
