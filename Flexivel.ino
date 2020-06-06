8c3dc47a1e51788f429c6f3b5ec62da52efcb273
Pedro Cruz <pedrohlcruz@gmail.com>
2020-06-05 22:30:38 -0300

    Cleaned code

--- Flexivel.ino
+++ Flexivel.ino
@@ -10,18 +10,19 @@ void setup()
 
   Serial.begin(115200);
   bluetooth.begin();
-  
+
 }
   
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   delay(1000);
+  
   //se existe algum dispositivo conectado
   if(Bluetooth::BLE::deviceConnected)
   {
     if(Serial.available() > 0)
     {
-      sensor.VoltametriaCiclica(25, 25, 25, 150, 12.5, 1);
+      sensor.VoltametriaCiclica(bluetooth,25, 25, 25, 150, 12.5, 1);
     }
 
     if(Serial.available() == 2)
@@ -29,19 +30,5 @@ void loop() {
       sensor.Amperometria(0, 2, 4, 10, 0.2);
     }
 
-    /*if(Serial.available() == 3)
-    {
-      VoltametriaDiferencial();
-    }
-
-    if(Serial.available() == 4)
-    {
-      VoltametriaQuadrada();
-    }
-
-    if(Serial.available() == 5)
-    {
-      Potenciometria();
-    }*/
   }
 }
