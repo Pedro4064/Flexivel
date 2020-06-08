506097e7c30130913e992c35b70018e3572578fe
Pedro Cruz <pedrohlcruz@gmail.com>
2020-06-08 10:57:59 -0300

    •[IMPLEMETED] The bluetooth data receivement. •[IMPLEMETED] Memory release to the system after each iteration.

--- Flexivel.ino
+++ Flexivel.ino
@@ -20,15 +20,27 @@ void loop() {
   //se existe algum dispositivo conectado
   if(Bluetooth::BLE::deviceConnected)
   {
-    if(Serial.available() > 0)
-    {
-      sensor.VoltametriaCiclica(bluetooth,25, 25, 25, 150, 12.5, 1);
-    }
+    // Receive the data 
+    double* incoming_data = bluetooth.receivedData();
 
-    if(Serial.available() == 2)
-    {
-      sensor.Amperometria(0, 2, 4, 10, 0.2);
-    }
+    // Check if there is actual data or if it's null 
+    if (incoming_data != NULL){
 
+      // If the data collection type is set to 0, it's VoltametriaCiclica
+      if(incoming_data[0] = 0)
+      {
+        sensor.VoltametriaCiclica(bluetooth,25, 25, 25, 150, 12.5, 1);
+      }
+
+      // If the data collection type is set to 1, it's Amperometria
+      else if(incoming_data[0] = 1)
+      {
+        sensor.Amperometria(bluetooth,0, 2, 4, 10, 0.2);
+      }
+
+
+      // After using the data in the BLE call, release the memory back to the system to avoid memory leaking
+      Bluetooth::BLE::releaseMemoryToSystem(incoming_data);
+    }
   }
 }
